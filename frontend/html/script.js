72bd5a50f44228da08cc5e5e05f615daf4f224d6
bhupirao <bhupirao646@gmail.com>
2026-02-01 08:09:06 +0530

    Day 1: frontend UI selector and resume upload page

---
+++ frontend/html/script.js
@@ -0,0 +1,9 @@
+function mockFill() {
+    document.getElementById("name").value = "Bhoop Singh";
+    document.getElementById("email").value = "bhoop@gmail.com";
+    document.getElementById("phone").value = "9876543210";
+    document.getElementById("experience").value = "3 Years";
+    document.getElementById("skills").value =
+      "Java, Spring Boot, SQL, HTML, CSS, JavaScript";
+  }
+  
